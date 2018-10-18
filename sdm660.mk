f7472e4435e3e2ca63aa9d9d5b64c8bb27db8cfb
Max Weffers <rcstar6696@gmail.com>
2018-10-18 16:09:51 +0200

    sdm660-common: overlay: Add freefrom Multiwindow managment

--- sdm660.mk
+++ sdm660.mk
@@ -250,6 +250,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
